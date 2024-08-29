fbb34eda714490c934b6cc1d9587b498969af280
ajovovich <ajovovich1@gmail.com>
2024-08-29 09:51:14 -0400

    First commit

---
+++ Assignment1.js
@@ -0,0 +1,20 @@
+// Task 1:Declare Variables
+
+let studentName; // String data type
+let studentAge;  // Number data type
+let studentGrade; // Number data type
+let isPresent;   // Boolean data type
+
+// Task 2: Assign Simple Values
+
+studentName = "Alice";
+studentAge = 15;
+studentGrade = 10;
+isPresent = true;
+
+// Task 3: Display the Student Information
+
+console.log("Student Name:", studentName);
+console.log("Student Age:", studentAge);
+console.log("Student Grade:", studentGrade);
+console.log("Attendance Status:", isPresent);
